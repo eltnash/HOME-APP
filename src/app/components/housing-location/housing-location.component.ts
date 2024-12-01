74fcc98d9fc592a3a0230c01331e88ddf9fdb3d0
Eren H <44721306+eltnash@users.noreply.github.com>
2024-12-01 17:32:23 +0200

    saved unsaved files

--- src/app/components/housing-location/housing-location.component.ts
+++ src/app/components/housing-location/housing-location.component.ts
@@ -10,9 +10,9 @@ import { HousingLocation } from '../../interfaces/housing-location'; //import th
   //inline structure
   `
       <section class="listing">
-        <img class="listing-photo">
-        <h2 class="listing-heading"></h2>
-        <p class="listing-location"></p>
+        <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+        <h2 class="listing-heading">{{housingLocation.name</h2>
+        <p class="listing-location">{{housingLocation.city}}.{{housingLocation.state}} </p>
       </section>
   `,
   //inline styling
--- src/app/interfaces/housing-location.ts
+++ src/app/interfaces/housing-location.ts
@@ -1,4 +1,7 @@
+// im guessing this is the interface that we can use to let the component know what type to data to expect?
 export interface HousingLocation {
+    // are these input properties that we can use to pass data to our component ?
+    // properties have key value pairs and the syntax below contans key value pair structure
     id:number,
     name:string,
     city:string,
@@ -8,3 +11,5 @@ export interface HousingLocation {
     wifi:boolean,
     laundry:boolean,
 }
+
+//inside the housing-location.component.ts file we want to pass the date to, we'l need to create an input decorator that tells angular that the property can be set in a template
